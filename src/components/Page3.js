94a8315374d63454dc7bb3cfb34df8285c1dbfde
HyoSung Bidol-Lee <hyosung11@me.com>
2019-12-24 13:59:42 -0800

    add disable button per page

--- src/components/Page3.js
+++ src/components/Page3.js
@@ -5,10 +5,11 @@ const Page3 = ({ onRouteChange }) =>
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
+      <h1 className="App-title">Welcome to Page 3</h1>
     </header>
     <button onClick={() => onRouteChange('page1')}>Page1</button>
     <button onClick={() => onRouteChange('page2')}>Page2</button>
+    <button className="disabled">Page3</button>
   </div>
 
 
