58e4db14700e28d326db22846493203e5b97181b
HyoSung Bidol-Lee <hyosung11@me.com>
2019-12-24 13:54:03 -0800

    links to pages work now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Page1 from './components/Page1';
@@ -19,17 +18,13 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-          <Page1 onRouteChange={this.onRouteChange}/>
-          <Page2 onRouteChange={this.onRouteChange}/>
-          <Page3 onRouteChange={this.onRouteChange}/>
-      </div>
-    );
+    if (this.state.route === 'page1') {
+      return <Page1 onRouteChange={this.onRouteChange}/>
+    } else if (this.state.route === 'page2') {
+      return <Page2 onRouteChange={this.onRouteChange}/>
+    } else if (this.state.route === 'page3') {
+      return <Page3 onRouteChange={this.onRouteChange}/>
+    }
   }
 }
 
